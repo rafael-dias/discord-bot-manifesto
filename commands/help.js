68a0d7e02cb251ccd55dec45b6605abe2b1d7ec5
Rafael Dias <rafael.arteg@gmail.com>
2021-07-15 20:18:07 -0300

    add help

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports.run = async(client, message, args) => {
+
+    // message.delete()
+    let msg = `
+    AJUDA MANIFESTO bot
+    comandos:
+    ´--utc´ exibe o horário utc atual
+    ´--na´ ´inicio´ ´fim´ exibe sequencia de números aleatórios 
+        ex: --na 1 5 // sera enviado uma sequencia semelhante a [4,5,2,1,3]
+    ´--arma´ exibe o nome de uma arma aleatória do albion
+    `
+    message.channel.send(msg);
+};
