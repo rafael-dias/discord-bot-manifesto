6f6a344b361cafce43bbdd9f0b600e3b6662d80a
Rafael Dias <rafael.arteg@gmail.com>
2021-07-15 20:41:56 -0300

    formatacao

--- commands/utc.js
+++ commands/utc.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 
-module.exports.run = async (client, message, args) => {
-  
-  function z(n) {
-  return ('0' + n).slice(-2);
-}
- 
-  const date = new Date()
-  let utc = z(date.getUTCHours()) + ':' + z(date.getUTCMinutes());
+module.exports.run = async(client, message, args) => {
 
-  
-  message.delete().catch(O_o => {});
-  message.channel.send(':alarm_clock: **'+utc+'** utc');
+    function z(n) {
+        return ('0' + n).slice(-2);
+    }
+
+    const date = new Date()
+    let utc = z(date.getUTCHours()) + ':' + z(date.getUTCMinutes());
+
+
+    message.delete().catch(O_o => {});
+    message.channel.send('` :alarm_clock: **' + utc + '** utc `');
 };
