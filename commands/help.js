75da833279a99779fb78817c6423a2d7f8671dd4
Rafael Dias <rafael.arteg@gmail.com>
2021-07-15 20:20:14 -0300

    makdown

--- commands/help.js
+++ commands/help.js
@@ -6,10 +6,10 @@ module.exports.run = async(client, message, args) => {
     let msg = `
     AJUDA MANIFESTO bot
     comandos:
-    ´--utc´ exibe o horário utc atual
-    ´--na´ ´inicio´ ´fim´ exibe sequencia de números aleatórios 
+    \`--utc\` exibe o horário utc atual
+    \`--na\` \`inicio\` \`fim\` exibe sequencia de números aleatórios 
         ex: --na 1 5 // sera enviado uma sequencia semelhante a [4,5,2,1,3]
-    ´--arma´ exibe o nome de uma arma aleatória do albion
+    \`--arma\` exibe o nome de uma arma aleatória do albion
     `
     message.channel.send(msg);
 };
