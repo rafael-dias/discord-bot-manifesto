b224446091f804e92b60b693f68dc033390a1050
Rafael Dias <rafael.arteg@gmail.com>
2021-07-15 22:32:03 -0300

    add reg

--- commands/reg.js
+++ commands/reg.js
@@ -1,6 +1,13 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, message, args) => {
+
+    if (args[0] || args[1] == '') {
+        return message.channel.send('Mencione um usuário e informe o nick do jogo').then(msg => msg.delete({
+            timeout: 3000
+        }))
+    }
+
     if (message.guild.members.cache.get(message.author.id).hasPermission("MANAGE_NICKNAMES") && message.guild.members.cache.get(message.author.id).hasPermission("CHANGE_NICKNAME")) {
 
         let target = message.mentions.users.first()
@@ -12,7 +19,13 @@ module.exports.run = async(client, message, args) => {
         member.roles.add(role);
 
         message.delete()
+        message.channel.send('✅ Usuário registrado!').then(msg => msg.delete({
+            timeout: 2000
+        }))
+
     } else {
-        message.channel.sendMessage("Você não tem permissão para registrar um membro");
+        message.channel.sendMessage("Você não tem permissão para registrar um membro").then(msg => msg.delete({
+            timeout: 2000
+        }));
     }
 };
